08eadfddba29f9f2802309f3badaa05380de7385
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-10-27 09:46:34 +0800

    Add Petchay Doctor apk release

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:potato_disease_classification/main.dart';
 import 'package:potato_disease_classification/pages/home_page.dart';
 
 void main() {
